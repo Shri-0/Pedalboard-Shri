cfd099b5015dc17656b657d928c6cd8087808369
Shri <shrinath.narayanan@gmail.com>
2023-10-17 18:03:14 -0700

    "manipulating file for PB later"

--- Main Audio Stuff/main.py
+++ Main Audio Stuff/main.py
@@ -20,4 +20,16 @@ print(t_audio)
 
 frames = obj.readframes(-1)   #reads all frames
 print(type(frames), type(frames[0]))
-print(len(frames))
+print(len(frames) / 2)
+
+obj.close()
+
+obj_new = wave.open("shri_new.wav", "wb")
+
+obj_new.setnchannels(2)
+obj_new.setsampwidth(2)
+obj_new.setframerate(16000.0)
+
+obj_new.writeframes(frames)
+
+obj_new.close()
