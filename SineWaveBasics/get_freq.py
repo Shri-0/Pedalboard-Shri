45ae1b6f8ced0f52e9563e3f2c45ae22b874ebb0
Shri <shrinath.narayanan@gmail.com>
2023-11-01 11:27:01 -0700

    "mapping change"

--- SineWaveBasics/get_freq.py
+++ SineWaveBasics/get_freq.py
@@ -5,7 +5,9 @@ import struct
 import matplotlib.pyplot as plt
 
 frame_rate = 48000.0
-infile = "test.wav"
+# infile = "test.wav"
+infile = "sineAmend.wav"
+
 num_samples = 48000
 wav_file = wave.open(infile, 'r')
 data = wav_file.readframes(num_samples)
