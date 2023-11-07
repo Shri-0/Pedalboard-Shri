fae6b3513a036499279da2ab365e54a3334f03f9
Shri <shrinath.narayanan@gmail.com>
2023-11-06 17:59:35 -0800

    "instructions"

--- PedalBoard/PB-EstablishedVST.py
+++ PedalBoard/PB-EstablishedVST.py
@@ -2,30 +2,27 @@ from pedalboard import Pedalboard, Chorus, Reverb, Delay, load_plugin
 from pedalboard.io import AudioFile
 from mido import Message
 
+# I am going to see if I can replicate the same results in an actual logic session using the same parameters here
+
 
 # load an instrument from the VSt Library
-instrument = load_plugin( "/Library/Audio/Plug-Ins/Components/Addictive Keys.component")
+instrument = load_plugin(
+    "/Library/Audio/Plug-Ins/Components/Addictive Keys.component")
 
 # dict_keys(['modulation_x', 'ch1_level', 'ch2_level', 'ch3_level', 'ch1_sendfx1', 'ch1_sendfx2', 'ch2_sendfx1', 'ch2_sendfx2', 'ch3_sendfx1', 'ch3_sendfx2', 'fx1_level', 'fx2_level', 'master_level', 'master_filtlo', 'master_filthi'])
 
 
-
 # Wrapping established 3rd party C++ plugins and processing audio files through here through there
 
-#SketchCassette
-SketchCassetteEffect = load_plugin("/Library/Audio/Plug-Ins/Components/SketchCassette II.component")
+# SketchCassette
+SketchCassetteEffect = load_plugin(
+    "/Library/Audio/Plug-Ins/Components/SketchCassette II.component")
 # dict_keys(['input_gain', 'output_gain', 'comp_dry_wet', 'dropout_intensity', 'flutter_rate', 'comp_amount', 'tape_quality', 'wow_rate', 'saturation_type', 'flutter_offset', 'wow_offset', 'age', 'flutter_rhythm', 'comp_brightness', 'wow_rhythm', 'dropout_depth', 'flutter_depth', 'flutter_shape', 'flanging', 'tape_hiss', 'nr_comp', 'tape_dry_wet', 'wow_depth', 'wow_shape', 'wow_flutter_mode', 'dropout_width', 'tape_type', 'tempo_sync', 'saturation'])
 
-#Valhalla Frequency Echo
-ValhallaFreqEchoeffect = load_plugin("/Library/Audio/Plug-Ins/Components/ValhallaFreqEcho.component")
+# Valhalla Frequency Echo
+ValhallaFreqEchoeffect = load_plugin(
+    "/Library/Audio/Plug-Ins/Components/ValhallaFreqEcho.component")
 # dict_keys(['wetdry', 'shift', 'delay', 'sync', 'feedback', 'lowcut', 'highcut', 'stereo'])
-
-
-
-
-
-
-
 
 
 '''
