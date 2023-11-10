b553177db8d4b1c3642392f8bbda0b07aad93f8e
Shri <shrinath.narayanan@gmail.com>
2023-11-09 18:29:50 -0800

    "set up"

--- PedalBoard/PB-LiveAudio.py
+++ PedalBoard/PB-LiveAudio.py
@@ -1,2 +1,12 @@
 from pedalboard import Pedalboard, Chorus, Compressor, Delay, Gain, Reverb, Phaser
 from pedalboard.io import AudioStream
+
+
+input_device_name = AudioStream.input_device_names[0]    #Monitor Camera
+output_device_name = AudioStream.output_device_names[1]	  #18i8 USB
+
+
+
+
+#print(input_device_name)
+#print(output_device_name)
--- PedalBoard/PB-ParallelChain.py
+++ PedalBoard/PB-ParallelChain.py
@@ -1,1 +1,15 @@
-from pedalboard import Pedalboard, Compressor, Delay, Distortion, Gain, PitchShift, Reverb, Mix
+from pedalboard import Pedalboard, Compressor, Delay, Distortion, Gain, PitchShift, Reverb, Mix, load_plugin
+
+
+initial_gain = Gain(gain_db=0)
+
+
+# SketchCassette
+SketchCassetteEffect = load_plugin(
+    "/Library/Audio/Plug-Ins/Components/SketchCassette II.component")
+# dict_keys(['input_gain', 'output_gain', 'comp_dry_wet', 'dropout_intensity', 'flutter_rate', 'comp_amount', 'tape_quality', 'wow_rate', 'saturation_type', 'flutter_offset', 'wow_offset', 'age', 'flutter_rhythm', 'comp_brightness', 'wow_rhythm', 'dropout_depth', 'flutter_depth', 'flutter_shape', 'flanging', 'tape_hiss', 'nr_comp', 'tape_dry_wet', 'wow_depth', 'wow_shape', 'wow_flutter_mode', 'dropout_width', 'tape_type', 'tempo_sync', 'saturation'])
+
+compress = Pedalboard([Compressor(threshold_db=-50, ratio=25, attack_ms=1, release_ms=100)])
+
+
+
