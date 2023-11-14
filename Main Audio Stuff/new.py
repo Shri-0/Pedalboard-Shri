6b8b2a7f9bbd977e56b1a5e7eab4543ad343afce
Shri <shrinath.narayanan@gmail.com>
2023-11-14 11:05:17 -0800

    "plot success"

--- Main Audio Stuff/new.py
+++ Main Audio Stuff/new.py
@@ -2,7 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import wave
 
-obj = wave.open("shri_new.wav", "rb")
+# obj = wave.open("shri_new.wav", "rb")
+
+############## WET VOICE SIGNAL ##############
+
+obj = wave.open("testing_change.wav", "rb")
 
 sample_freq = obj.getframerate()
 n_samples = obj.getnframes()
@@ -12,12 +16,20 @@ obj.close()
 
 t_audio = n_samples / sample_freq
 
-print(t_audio)
+# print(t_audio)
 
 
-signal_array = np.frombuffer(signal_wave, dtype=np.int16)
+signal_array = np.frombuffer(signal_wave, dtype=np.int16)  # y
 
-times = np.linspace(0, t_audio, num=n_samples)
+times = np.linspace(0, t_audio, num=n_samples*2)  # x
+#times = np.linspace(0, t_audio, num=n_samples)  # x             - For Mono Files
+
+print(signal_array)
+print(times)
+
+x = np.array(times)
+y = np.array(signal_array)
+
 
 plt.figure(figsize=(15, 5))
 plt.plot(times, signal_array)
@@ -25,4 +37,31 @@ plt.title("Audio signal")
 plt.ylabel("Signal Wave")
 plt.xlabel("Time(s)")
 plt.xlim(0, t_audio)
+plt.show()
+
+
+
+####################  DRY VOICE SIGNAL   ##############################
+
+obj_two = wave.open("test_message.wav", "rb")
+
+sample_freq_two = obj_two.getframerate()
+n_samples_two = obj_two.getnframes()
+signal_wave_two = obj_two.readframes(-1)
+
+obj_two.close()
+
+t_audio_two = n_samples_two / sample_freq_two
+
+signal_array_two = np.frombuffer(signal_wave_two, dtype=np.int16)  # y
+
+times_two = np.linspace(0, t_audio_two, num=n_samples_two*2)  # x
+
+
+plt.figure(figsize=(15, 5))
+plt.plot(times_two, signal_array_two)
+plt.title("Audio signal")
+plt.ylabel("Signal Wave")
+plt.xlabel("Time(s)")
+plt.xlim(0, t_audio_two)
 plt.show()
--- PedalBoard/PB-ScratchVST.py
+++ PedalBoard/PB-ScratchVST.py
@@ -1,5 +1,8 @@
 from pedalboard import Pedalboard, Chorus, Reverb, Delay, load_plugin
 from pedalboard.io import AudioFile
+import wave
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 effect = load_plugin("/Library/Audio/Plug-Ins/Components/Tent.component")
@@ -7,7 +10,7 @@ effect = load_plugin("/Library/Audio/Plug-Ins/Components/Tent.component")
 # Wrapping a sample C++ plugin and processing the sine wave through there
 
 
-print(effect.parameters.keys())
+# print(effect.parameters.keys())
 
 # dict_keys(['par0', 'par1', 'par2', 'par3', 'par4', 'par5', 'par6', 'par7', 'par8', 'par9', 'toggle0', 'toggle1', 'toggle8', 'toggle9', 'par10', 'par11',
 #          'par12', 'par13', 'par14', 'par15', 'par16', 'calinput', 'sidebutton0', 'sidebutton1', 'sidebutton2', 'fft_window', 'fft_size', 'scalefactor'])
@@ -46,3 +49,41 @@ with AudioFile('test_message.wav') as f:
             effected = effect(chunk, f.samplerate, reset=False)
 
             o.write(effected)
+
+
+######### Plotting The Wave #########
+
+# encoding - pcm_s16le
+# format - s16
+# number of channels - 2 (stereo)
+# sample_rate - 16000
+# file_size - 704104 bytes
+# duration - 11s
+
+
+obj = wave.open("Test_Two.WAV", "rb")
+
+
+print("Number of channels", obj.getnchannels())  # 2
+print("sample width", obj.getsampwidth())  # 2
+print("frame rate", obj.getframerate())  # 16000
+print("Number of frames", obj.getnframes())  # 176000
+
+
+print("parameters", obj.getparams())
+# comptype = NONE, comname= "not compressed"
+
+frames = obj.readframes(-1)  # reads all frames
+print(type(frames), type(frames[0]))
+print(len(frames) / 2)                    #352000.0 frames
+
+obj_new = wave.open("testing_change.wav", "wb")
+
+obj_new.setnchannels(2)
+obj_new.setsampwidth(2)
+obj_new.setframerate(16000.0)
+obj_new.writeframes(frames)
+
+obj_new.close()
+
+####### Soundfile has been converted ###
