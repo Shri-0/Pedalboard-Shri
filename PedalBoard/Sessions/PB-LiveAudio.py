13bd6cb637df93f280cb8bd2d3efb4587001170c
Shri <shrinath.narayanan@gmail.com>
2023-12-07 09:54:53 -0800

    working on dynamically recording for live stream

--- PedalBoard/Sessions/PB-LiveAudio.py
+++ PedalBoard/Sessions/PB-LiveAudio.py
@@ -1,5 +1,82 @@
 from pedalboard import Pedalboard, Chorus, Compressor, Delay, Gain, Reverb, Phaser, load_plugin, Convolution
 from pedalboard.io import AudioStream
+import wave
+import pyaudio
+
+
+input_device_name = AudioStream.input_device_names[1]
+output_device_name = AudioStream.output_device_names[1]
+
+'''
+FRAMES_PER_BUFFER = 3200
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+# CHANNELS = 1
+
+RATE = 16000
+'''
+
+
+
+def liveAudio():
+	with AudioStream(
+		input_device_name, output_device_name
+
+	) as stream:
+		stream.plugins = Pedalboard([
+			Chorus(),
+			Phaser(),
+			Reverb(room_size=0.25),
+		]
+                              )
+	input("Press enter to stop streaming...")
+
+
+'''
+	frames = []
+
+    ##### insert loop ####
+
+
+
+    ##### insert loop ####
+
+
+	obj = wave.open("demoV1.wav", "wb")
+	obj.setnchannels(CHANNELS)
+	obj.setsampwidth(stream.get_sample_size(FORMAT))
+	obj.setframerate(RATE)
+	obj.writeframes(b"".join(frames))
+	obj.close()
+ 
+'''
+
+
+
+print(input_device_name)
+print(output_device_name)
+
+
+def main():
+    liveAudio()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 '''
 SketchCassetteEffect = load_plugin(
@@ -15,51 +92,14 @@ SketchCassetteEffect.tape_quality = "Standard"
 SketchCassetteEffect.age = 0.8
 SketchCassetteEffect.output_gain = 0.4
 
-#5.0
-#Standard
-#0.0
-#0.0
+# 5.0
+# Standard
+# 0.0
+# 0.0
 '''
 
 '''
 Was able to run synth into Audio interface and out through monitors when hitting enter
 
 Still want to see if an external plugin can be run through this in the future
-
 '''
-
-
-input_device_name = AudioStream.input_device_names[1]
-output_device_name = AudioStream.output_device_names[1]
-
-
-
-
-def liveAudio():
-	with AudioStream(
-		input_device_name, output_device_name
-
-	) as stream:
-		stream.plugins = Pedalboard([
-			Chorus(),
-			Phaser(),
-			Reverb(room_size=0.25),
-		])
-
-		input("Press enter to stop streaming...")
-
-
-
-print(input_device_name)
-print(output_device_name)
-
-
-
-
-
-def main():
-    liveAudio()
-
-
-if __name__ == "__main__":
-    main()
